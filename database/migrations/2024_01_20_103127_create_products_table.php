487543f5632b20abc64784b8e1b4b1b879cb230f
mio-yamamoto <>
2024-01-20 19:46:58 +0900

    update: migration - products

--- database/migrations/2024_01_20_103127_create_products_table.php
+++ database/migrations/2024_01_20_103127_create_products_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('company_id');
+            $table->string('product_name');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->text('comment')->nullable();
+            $table->string('img_path')->nullable();
             $table->timestamps();
         });
     }
