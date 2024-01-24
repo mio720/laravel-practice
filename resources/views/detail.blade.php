9a0a486065a038f5c22285ba39a237ab158382c9
mio-yamamoto <>
2024-01-25 08:25:07 +0900

    add: view - detail

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', '商品情報編集画面')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">商品情報詳細画面</div>
+
+                <div class="card-body">
+                    <!-- ここに追加 -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
