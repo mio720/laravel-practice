5a40fc8ea58272aaa88210533a4cb31b72825b79
mio-yamamoto <>
2024-02-02 08:24:18 +0900

    update: view - edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -28,7 +28,7 @@
                         <div class="row mb-3">
                             <p class="col-sm-2 col-form-label">商品名</p>
                             <div class="col-sm-10">
-                                <input type="text" id="txtProductName" name="product_name" @if ($errors->has('product_name')) class="form-control is-invalid" @else class="form-control" @endif value="{{ $product->product_name }}">
+                                <input type="text" id="txtProductName" name="product_name" @if ($errors->has('product_name')) class="form-control is-invalid" @else class="form-control" @endif value="{{ old('product_name', $product->product_name) }}">
                             </div>
                         </div>
                         <div class="row mb-3">
@@ -37,7 +37,7 @@
                                 <select id="drpCompanyId" name="company_id" @if ($errors->has('company_id')) class="form-select is-invalid" @else class="form-select" @endif>
                                     <option value=""></option>
                                     @foreach ($companies as $company)
-                                    <option value="{{ $company->id }}" @if ((int)old('company_id')===$company->id) selected @endif >{{ $company->company_name }}</option>
+                                    <option value="{{ $company->id }}" @if ((int)old('company_id', $product->company_id) === $company->id) selected @endif >{{ $company->company_name }}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -45,19 +45,19 @@
                         <div class="row mb-3">
                             <p class="col-sm-2 col-form-label">価格</p>
                             <div class="col-sm-10">
-                                <input type="number" id="numPrice" name="price" @if ($errors->has('price')) class="form-control is-invalid" @else class="form-control" @endif value="{{ old('price') }}">
+                                <input type="number" id="numPrice" name="price" @if ($errors->has('price')) class="form-control is-invalid" @else class="form-control" @endif value="{{ old('price', $product->price) }}">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <p class="col-sm-2 col-form-label">在庫数</p>
                             <div class="col-sm-10">
-                                <input type="number" id="numStock" name="stock" @if ($errors->has('stock')) class="form-control is-invalid" @else class="form-control" @endif value="{{ old('stock') }}">
+                                <input type="number" id="numStock" name="stock" @if ($errors->has('stock')) class="form-control is-invalid" @else class="form-control" @endif value="{{ old('stock', $product->stock) }}">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <p class="col-sm-2 col-form-label">コメント</p>
                             <div class="col-sm-10">
-                                <textarea id="areaComment" name="comment" class="form-control" style="height: 100px">{{ old('comment') }}</textarea>
+                                <textarea id="areaComment" name="comment" class="form-control" style="height: 100px">{{ old('comment', $product->comment) }}</textarea>
                             </div>
                         </div>
                         <div class="d-flex justify-content-end">
